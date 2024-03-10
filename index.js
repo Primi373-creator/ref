fb699aec65f84600ad2f210699a3630e024c0929
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-03-11 07:28:24 +1030

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const filePath = './file.txt';
 const TOKEN = "7068252272:AAEmmyT78tq3WcbalMwR2fD2Dlhjr1h7XNM";
 const MONGODB_URI =
   "mongodb+srv://uploader2:uploader2@uploader2.uhnmx1u.mongodb.net/?retryWrites=true&w=majority&appName=uploader2";
-const CHANNEL_NAME = "@obasuyi4";
+const CHANNEL_NAME = "@obasuyi5";
 const ADMIN_IDS = [5958051599];
 const client = new MongoClient(MONGODB_URI, {
   useNewUrlParser: true,

12424537f8ada9e0317c6f8f3e7f567329ee5e20
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-03-11 07:18:03 +1030

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const filePath = './file.txt';
 const TOKEN = "7068252272:AAEmmyT78tq3WcbalMwR2fD2Dlhjr1h7XNM";
 const MONGODB_URI =
   "mongodb+srv://uploader2:uploader2@uploader2.uhnmx1u.mongodb.net/?retryWrites=true&w=majority&appName=uploader2";
-const CHANNEL_NAME = "@hackersssd";
+const CHANNEL_NAME = "hackersssd";
 const ADMIN_IDS = [6341138384];
 
 const msgId = 6341138384; 
@@ -48,7 +48,7 @@ bot.start(async (ctx) => {
 
   if (!isSubscribed) {
     ctx.reply(
-      `Hello, ${username}!, To use this bot, you must be subscribed to our channel: ${CHANNEL_NAME}\n\n` +
+      `Hello, ${username}!, To use this bot, you must be subscribed to our channel: @${CHANNEL_NAME}\n\n` +
       "Click the link below to subscribe:\n" +
       `t.me/${CHANNEL_NAME}`,
     );
